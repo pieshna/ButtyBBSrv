6c136bf68e111cccf5011b17ceee4b564c8e8c6c
Jose Cabrera <cabreraj525@gmail.com>
2023-10-22 14:33:56 -0600

    agregamos descripcion a schema

--- src/stock/stock.schema.ts
+++ src/stock/stock.schema.ts
@@ -5,6 +5,7 @@ export const stockSchema = z.object({
   producto_id: z.number(),
   precio_venta: z.number(),
   unidades: z.number(),
+  descripcion: z.string().optional(),
   created_at: z.date().optional(),
   updated_at: z.date().optional()
 })
