d9a5145ef55bdb53f9bcb3e66708a2c7a28ae12c
Jose Cabrera <cabreraj525@gmail.com>
2023-10-28 21:59:22 -0600

    cambiamos estructura de prodcuto

--- src/productos/producto.schema.ts
+++ src/productos/producto.schema.ts
@@ -3,7 +3,7 @@ import { z } from 'zod'
 export const productoSchema = z.object({
   id: z.number().optional(),
   nombre: z.string().max(50),
-  imagen: z.string(),
+  imagen: z.string().optional(),
   precio_compra: z.number(),
   created_at: z.date().optional(),
   updated_at: z.date().optional()
